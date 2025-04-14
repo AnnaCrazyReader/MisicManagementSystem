6028f3cbf945ea96b543c6bd5e2e1296377cb56a
Abramenkova Anna <abramenkova@dlit.dp.ua>
2025-04-14 22:42:53 +0300

    Створено моделі даних Artist та Song

---
+++ MisicManagementSystem/Models/Artist.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MisicManagementSystem.Models
+{
+    internal class Artist
+    {
+        public int artist_id { get; set; }
+        public string artist_name { get; set; }
+        public string country_origin { get; set; }
+        public int birth_year { get; set; }
+        public string musical_genre { get; set; }
+        public string profile_photo { get; set; }
+
+        // Зв'язок один-до-багатьох
+        public virtual List<Song> Songs { get; set; }
+    }
+}
